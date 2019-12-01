3c3504b9bada36e3458ef4b55e73cd0731dba90e
bigtor <bigtor@arch.home>
2019-12-01 12:15:46 +0100

    extract fuel function and partial results to reuse in part2

--- 2019/src/bin/day01.rs
+++ 2019/src/bin/day01.rs
@@ -3,10 +3,16 @@ use std::io::{self, Read};
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let res1 : u32 = input.lines()
-                          .filter_map(|s| s.parse::<u32>().ok())
-                          .map(|mass| (mass/3) -2)
-                          .sum();
+    let fuels = input.lines()
+                     .filter_map(|s| s.parse().ok())
+                     .map(calculate_fuel)
+                     .collect::<Vec<u32>>();
+    let res1: u32 = fuels.into_iter().sum();
     println!("{}", res1);
     Ok(())
+}
+
+fn calculate_fuel(mass: u32) -> u32 {
+    let partial_res = mass/3;
+    if partial_res > 2 { partial_res - 2 } else { 0 }
 }
