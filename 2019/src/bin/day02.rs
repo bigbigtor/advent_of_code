1aeff49beff111d3d4b0fd9ba8da23df4aab8ef1
bigtor <bigtor@arch.home>
2019-12-03 22:44:02 +0100

    refactor to allow part 2

--- 2019/src/bin/day02.rs
+++ 2019/src/bin/day02.rs
@@ -1,13 +1,52 @@
 use std::io::{self, Read};
 
+struct Computer<'a> {
+    memory: &'a mut Vec<usize>,
+    ic: usize,
+}
+
+impl<'a> Computer<'a> {
+    fn run(self: &mut Self, input: &Vec<usize>, noun: usize, verb: usize) -> Vec<usize> {
+        self.memory.clear();
+        self.memory.append(&mut input.clone());
+        self.memory[1] = noun;
+        self.memory[2] = verb;
+        self.ic = 0;
+        loop {
+            let opcode = self.memory[self.ic];
+            match opcode {
+                1 => {
+                    let op1_addr = self.memory[self.ic + 1];
+                    let op2_addr = self.memory[self.ic + 2];
+                    let dst_addr = self.memory[self.ic + 3];
+                    self.memory[dst_addr] = self.memory[op1_addr] + self.memory[op2_addr];
+                    self.ic += 4;
+                },
+                2 => {
+                    let op1_addr = self.memory[self.ic + 1];
+                    let op2_addr = self.memory[self.ic + 2];
+                    let dst_addr = self.memory[self.ic + 3];
+                    self.memory[dst_addr] = self.memory[op1_addr] * self.memory[op2_addr];
+                    self.ic += 4;
+                },
+                99 => break,
+                o => panic!("Wrong opcode{}", o),
+            }
+        }
+        self.memory.clone()
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    let mut program = parse(buffer);
-    program[1] = 12;
-    program[2] = 2;
-    let program = run(&program);
-    println!("{}", program[0]);
+    let program = parse(buffer);
+    let mut computer = Computer {
+        memory: &mut Vec::new(),
+        ic: 0,
+    };
+    let output = computer.run(&program, 12, 2);
+    println!("{}", output[0]);
     Ok(())
 }
 
@@ -17,20 +56,7 @@ fn parse(input: String) -> Vec<usize> {
          .collect()
 }
 
-fn run(program: &Vec<usize>) -> Vec<usize> {
-    let mut result = program.clone();
-    program.chunks(4)
-           .take_while(|s| s[0] != 99)
-           .for_each(|c| {
-               let (opcode, src1, src2, dst) = (c[0], c[1], c[2], c[3]);
-               match opcode {
-                   1 => result[dst] = result[src1] + result[src2],
-                   2 => result[dst] = result[src1] * result[src2],
-                   o => panic!("Wrong opcode{}", o),
-               }
-           });
-    result
-}
+
 
 #[cfg(test)]
 mod tests {
@@ -38,29 +64,45 @@ mod tests {
 
     #[test]
     fn test_run_1() {
+        let mut computer: Computer = Computer {
+            memory: &mut Vec::new(),
+            ic: 0,
+        };
         let input = vec![1,0,0,0,99];
         let output = vec![2,0,0,0,99];
-        assert_eq!(run(&input), output);
+        assert_eq!(computer.run(&input, 0, 0), output);
     }
 
     #[test]
     fn test_run_2() {
+        let mut computer: Computer = Computer {
+            memory: &mut Vec::new(),
+            ic: 0,
+        };
         let input = vec![2,3,0,3,99];
         let output = vec![2,3,0,6,99];
-        assert_eq!(run(&input), output);
+        assert_eq!(computer.run(&input, 3, 0), output);
     }
 
     #[test]
     fn test_run_3() {
+        let mut computer: Computer = Computer {
+            memory: &mut Vec::new(),
+            ic: 0,
+        };
         let input = vec![2,4,4,5,99,0];
         let output = vec![2,4,4,5,99,9801];
-        assert_eq!(run(&input), output);
+        assert_eq!(computer.run(&input, 4, 4), output);
     }
 
     #[test]
     fn test_run_4() {
+        let mut computer: Computer = Computer {
+            memory: &mut Vec::new(),
+            ic: 0,
+        };
         let input = vec![1,1,1,4,99,5,6,0,99];
         let output = vec![30,1,1,4,2,5,6,0,99];
-        assert_eq!(run(&input), output);
+        assert_eq!(computer.run(&input, 1, 1), output);
     }
 }
