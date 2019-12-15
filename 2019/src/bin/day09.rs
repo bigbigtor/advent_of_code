20ddf6311cc333bd1937a423de192264fa69e576
bigtor <bigtor@arch.home>
2019-12-15 22:06:14 +0100

    add day9 part 2

--- 2019/src/bin/day09.rs
+++ 2019/src/bin/day09.rs
@@ -3,15 +3,20 @@ use advent_of_code_2019::computer::{Computer, Status};
 
 fn main() -> io::Result<()> {
     let input = read_input();
+    run_boost(&input, 1).iter().for_each(|out| println!("{}", out));
+    run_boost(&input, 2).iter().for_each(|out| println!("{}", out));
+    Ok(())
+}
+
+fn run_boost(input: &Vec<i64>, mode: i64) -> Vec<i64> {
     let mut computer = Computer::new();
     computer.load(&input);
-    computer.input.push(1);
+    computer.input.push(mode);
     loop {
         let status = computer.run();
         if status == Status::Halt { break; }
     }
-    computer.output.iter().for_each(|out| println!("{}", out));
-    Ok(())
+    computer.output
 }
 
 fn read_input() -> Vec<i64> {
