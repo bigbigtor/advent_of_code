b2fb6adec7c732b1a2360468c6d20e3d9694b8e6
bigtor <bigtor@arch.home>
2019-12-19 00:21:13 +0100

    add day 10 part 2

--- 2019/src/bin/day10.rs
+++ 2019/src/bin/day10.rs
@@ -1,20 +1,48 @@
 use std::io::{self, Read};
-use std::collections::HashSet;
+use std::collections::{HashSet, BTreeMap};
 use std::f32;
 use advent_of_code_2019::point::Point;
 
 fn main() -> io::Result<()> {
     let input = read_input()?;
     let asteroids = get_asteroids(&input);
-    let res1 = get_best_count(&asteroids);
-    println!("{}", res1);
+    let (best_asteroid, res1) = get_best_count(&asteroids);
+    let destroyed = get_nth_destroyed_asteroid(&best_asteroid, &asteroids, 200);
+    let res2 = (destroyed.x * 100) + destroyed.y;
+    println!("{}\n{}", res1, res2);
     Ok(())
 }
 
-fn get_best_count(asteroids: &Vec<Point>) -> usize {
+fn get_nth_destroyed_asteroid(ast: &Point, asteroids: &Vec<Point>, n: usize) -> Point {
+    let mut map = get_angle2distance_map(&ast, &asteroids);
+    map.iter_mut().for_each(|(_k, v)| v.sort_by_key(|(_p, d)| *d));
+    let mut destroyed = 0;
+    loop {
+        for (_k, v) in map.iter_mut().rev() {
+            if v.len() > 0 {
+                let (latest, _) = v.remove(0);
+                destroyed += 1;
+                if destroyed == n { return latest; }
+            }
+        }
+    }
+}
+
+fn get_angle2distance_map(ast: &Point, asteroids: &Vec<Point>) -> BTreeMap<i32, Vec<(Point, i16)>> {
     asteroids.iter()
-             .map(|p| get_num_angles(&p, &asteroids))
-             .max()
+             .filter(|&p| p != ast) //skip myself
+             .map(|p| (p, get_angle(&ast, &p), Point::get_manhattan_distance(&ast, &p)))
+             .fold(BTreeMap::new(), |mut acc, (p, a, d)| {
+                 let entry = acc.entry(a).or_insert(Vec::new());
+                 entry.push((*p, d));
+                 acc
+             })
+}
+
+fn get_best_count(asteroids: &Vec<Point>) -> (Point, usize) {
+    asteroids.iter()
+             .map(|p| (*p, get_num_angles(&p, &asteroids)))
+             .max_by_key(|(_p, num)| *num)
              .unwrap()
 }
 
@@ -67,7 +95,7 @@ mod tests {
 ...##";
         let asteroids = get_asteroids(input);
         let output = 8;
-        let res1 = get_best_count(&asteroids);
+        let (_, res1) = get_best_count(&asteroids);
         assert_eq!(res1, output);
     }
 
@@ -85,7 +113,7 @@ mod tests {
 .#....####";
         let asteroids = get_asteroids(input);
         let output = 33;
-        let res1 = get_best_count(&asteroids);
+        let (_, res1) = get_best_count(&asteroids);
         assert_eq!(res1, output);
     }
 
@@ -103,7 +131,7 @@ mod tests {
 .####.###.";
         let asteroids = get_asteroids(input);
         let output = 35;
-        let res1 = get_best_count(&asteroids);
+        let (_, res1) = get_best_count(&asteroids);
         assert_eq!(res1, output);
     }
 
@@ -121,7 +149,7 @@ mod tests {
 .....#.#..";
         let asteroids = get_asteroids(input);
         let output = 41;
-        let res1 = get_best_count(&asteroids);
+        let (_, res1) = get_best_count(&asteroids);
         assert_eq!(res1, output);
     }
     #[test]
@@ -148,7 +176,11 @@ mod tests {
 ###.##.####.##.#..##";
         let asteroids = get_asteroids(input);
         let output = 210;
-        let res1 = get_best_count(&asteroids);
+        let output2 = 802;
+        let (best, res1) = get_best_count(&asteroids);
+        let destroyed = get_nth_destroyed_asteroid(&best, &asteroids, 200);
+        let res2 = (destroyed.x * 100) + destroyed.y;
         assert_eq!(res1, output);
+        assert_eq!(res2, output2);
     }
 }
