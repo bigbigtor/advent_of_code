a4e730ec4d053bfc83a8e3df9143197f6197eba0
bigbigtor <bigbigtor@users.noreply.github.com>
2020-02-14 23:01:31 +0100

    solve day14 part2

--- 2019/src/bin/day14.rs
+++ 2019/src/bin/day14.rs
@@ -6,6 +6,23 @@ fn main() -> io::Result<()> {
     io::stdin().read_to_string(&mut input)?;
     let nanofactory = Nanofactory::from(input.as_str());
     let part1 = nanofactory.find_min_ore_qty(1);
+    let part2 = find_max_fuel(&nanofactory);
     println!("{}", part1);
+    println!("{}", part2);
     Ok(())
+}
+
+fn find_max_fuel(factory: &Nanofactory) -> u64 {
+    let ore = 1_000_000_000_000;
+    let mut min = ore / factory.find_min_ore_qty(1);
+    let mut max = min * 2;
+    while (max - min) > 1 {
+        let middle = (min + max) / 2;
+        if factory.find_min_ore_qty(middle) < ore {
+            min = middle;
+        } else {
+            max = middle;
+        }
+    }
+    min
 }
