39012b6806baf6a0b46169eee2c8a182a4261d83
bigbigtor <bigbigtor@users.noreply.github.com>
2020-02-21 00:42:34 +0100

    add day16 part2

--- 2019/src/bin/day16.rs
+++ 2019/src/bin/day16.rs
@@ -4,7 +4,30 @@ fn main() -> io::Result<()> {
     let input = read_input()?;
     let res1 = apply_fft_algorithm(&input, 100);
     println!("{:?}", res1);
+    let res2 = get_message(&input, 100);
+    println!("{:?}", res2);
     Ok(())
+}
+
+fn get_message(signal: &Vec<i32>, num_phases: u8) -> u32 {
+    let offset = signal.iter().take(7).fold(0, |acc, d| acc * 10 + d) as usize;
+    let end = signal.len() * 10000;
+    let mut message = (0..(end - offset)).map(|i| signal[(offset + i) % signal.len()])
+                                         .collect::<Vec<i32>>();
+    for _ in 0..num_phases {
+        let mut sum: i32 = 0;
+        for i in (0..(end-offset)).rev() {
+            sum += message[i];
+            message[i] = sum.abs() % 10;
+        }
+    }
+    message.iter()
+          .take(8)
+          .map(|d| d.to_string())
+          .collect::<Vec<_>>()
+          .join("")
+          .parse()
+          .unwrap()
 }
 
 fn apply_fft_algorithm(signal: &Vec<i32>, num_phases: u8) -> u32 {
@@ -97,5 +120,26 @@ mod tests {
         let output = 01029498;
         let signal = vec![1, 2, 3, 4, 5, 6, 7, 8];
         assert_eq!(apply_fft_algorithm(&signal, 4), output);
+    }
+
+    #[test]
+    fn get_message_1() {
+        let output = 84462026;
+        let signal = vec![0,3,0,3,6,7,3,2,5,7,7,2,1,2,9,4,4,0,6,3,4,9,1,5,6,5,4,7,4,6,6,4];
+        assert_eq!(get_message(&signal, 100), output);
+    }
+
+    #[test]
+    fn get_message_2() {
+        let output = 78725270;
+        let signal = vec![0,2,9,3,5,1,0,9,6,9,9,9,4,0,8,0,7,4,0,7,5,8,5,4,4,7,0,3,4,3,2,3];
+        assert_eq!(get_message(&signal, 100), output);
+    }
+
+    #[test]
+    fn get_message_3() {
+        let output = 53553731;
+        let signal = vec![0,3,0,8,1,7,7,0,8,8,4,9,2,1,9,5,9,7,3,1,1,6,5,4,4,6,8,5,0,5,1,7];
+        assert_eq!(get_message(&signal, 100), output);
     }
 }
