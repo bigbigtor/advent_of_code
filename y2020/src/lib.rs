ef668b5ee32ccbf0da89a04fa560a7b15fb046fa
bigbigtor <bigbigtor@users.noreply.github.com>
2021-01-09 16:16:11 -0600

    day 7 part 1

--- y2020/src/lib.rs
+++ y2020/src/lib.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
 pub mod expense_report;
 pub mod password_database;
 pub mod trajectory;
 pub mod plane;
 pub mod customs;
+pub mod luggage;
